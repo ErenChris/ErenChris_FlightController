b6362154cd9b548f9271c8b8c5235b34c0542b1e
ErenChris <55536895+ErenChris@users.noreply.github.com>
2021-04-20 22:24:53 +0800

    Update FlightContoller.ino

--- FlightContoller.ino
+++ FlightContoller.ino
@@ -40,9 +40,14 @@
 //#define IRQ 
 #define CE  7
 #define CSN 8
+//定义状态寄存器的相关位
+#define RX_DR 0x40
+#define TX_DS 0x20
+#define MAX_RT 0x10
 
 byte RX_Data_Package[32];
 byte TX_Data_Package[32];
+byte TX_RX_Address = 0x3443101001;
 
 void setup() {
   Serial.begin(9600);
@@ -58,14 +63,30 @@ void setup() {
   //SPI_Reg_Write();
   Serial.println(int(0x6e));
   delay(500);
+
+  for(int i = 0; i < 32; i++)
+  {
+    TX_Data_Package[i] = 1;
+  }
+
+  Write_TX_Data(TX_Data_Package);
+
+  delay(100);
+
+  Read_RX_Data(TX_Data_Package);
+
+  for(int c = 0; c < 32; c++)
+  {
+    Serial.print(RX_Data_Package[c]);
+  }
 }
 
 void loop() 
 {
-  byte regValue_nrf;
-  regValue_nrf=SPI_Reg_Read(CONFIG);
-  Serial.println((byte)regValue_nrf);
-  delay(10);
+//  byte regValue_nrf;
+//  regValue_nrf=SPI_Reg_Read(CONFIG);
+//  Serial.println((byte)regValue_nrf);
+//  delay(10);
 }
 
 void init_nrf24l01()
@@ -134,14 +155,88 @@ byte SPI_Reg_Read(byte address)
   return outputValue;
 }
 
-void init_TX_Mode()
+void Write_TX_Data(byte *TX_Data)
 {
-  
+  digitalWrite(CSN,0);
+  SPI_Write(WR_TX_PLOAD);
+  for(int i = 0; i < 32; i++)
+  {
+    if(TX_Data[i])
+     {
+      digitalWrite(MOSI,1);
+     }
+     else
+     {
+      digitalWrite(MOSI,0);
+     }
+  }
+  digitalWrite(SCK,1);
+  delay(1);
+  digitalWrite(SCK,0);
+  delay(1);
+  digitalWrite(CSN,1);
 }
 
-void init_RX_Mode()
+void Read_RX_Data(byte *RX_Data)
 {
-  
+  digitalWrite(CSN,0);
+  SPI_Write(RD_RX_PLOAD);
+  for(int i = 0; i < 32; i++)
+  {
+    if(digitalRead(MISO))
+    {
+        RX_Data[i] = 1;
+    }
+    else
+    {
+        RX_Data[i] = 0;
+    }
+  }
+}
+
+void init_TX_Data()
+{
+  byte Reg_Status;
+
+  Reg_Status = SPI_Reg_Read(STATUS);
+  Serial.println(Reg_Status);
+  if(Reg_Status & TX_DS)
+  {
+    SPI_Write(FLUSH_TX);
+    Write_TX_Data(TX_Data_Package);
+  }
+  if(Reg_Status & MAX_RT)
+  {
+    SPI_Write(FLUSH_TX);
+    Write_TX_Data(TX_Data_Package);
+  }
+
+  SPI_Reg_Write(STATUS,Reg_Status);
+  delay(10);
+}
+
+void init_RX_Data()
+{
+  byte Reg_Status;
+
+  Reg_Status = SPI_Reg_Read(STATUS);
+  if(Reg_Status & RX_DR)
+  {
+    Read_RX_Data(RX_Data_Package);
+    SPI_Write(FLUSH_RX);
+  }
+
+  SPI_Reg_Write(STATUS,Reg_Status);
+}
+
+void init_TX_Mode()
+{
+  digitalWrite(CSN,0);
+  SPI_Write(TX_ADDR);
+  for(int i=0; i<40; i++)
+  {
+    
+  }
 }
 
 //byte nRF24L01_SPI_RW(byte dat)
@@ -186,41 +281,3 @@ void init_RX_Mode()
 //  return(reg_val);
 //}
 
-void Write_TX_Data(byte *TX_Data)
-{
-  digitalWrite(CSN,0);
-  SPI_Write(WR_TX_PLOAD);
-  for(int i = 0; i < 32; i++)
-  {
-    if(TX_Data[i])
-     {
-      digitalWrite(MOSI,1);
-     }
-     else
-     {
-      digitalWrite(MOSI,0);
-     }
-  }
-  digitalWrite(SCK,1);
-  delay(1);
-  digitalWrite(SCK,0);
-  delay(1);
-  digitalWrite(CSN,1);
-}
-
-void Read_RX_Data(byte *RX_Data)
-{
-  digitalWrite(CSN,0);
-  SPI_Write(RD_RX_PLOAD);
-  for(int i = 0; i < 32; i++)
-  {
-    if(digitalRead(MISO))
-    {
-        RX_Data[i] = 1;
-    }
-    else
-    {
-        RX_Data[i] = 0;
-    }
-  }
-}
